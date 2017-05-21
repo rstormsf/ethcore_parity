5d009e1e1d7100ec95a0065436771bc7e3afacda
Iyad Aljayousi <iyad.aljayousi@cloud-track.com.au>
2017-05-22 00:39:09 +1000

    replaced ALB with ELB

--- scripts/generate_token.sh
+++ scripts/generate_token.sh
@@ -2,10 +2,8 @@
 echo "Content-type: text/html"
 echo ""
 
-export container_id=`docker run -itd -v /root/.local/share/io.parity.ethereum/:/mnt/parity/ ethcore/parity:nightly --log-file /var/log/parity --logging trace --chain kovan --base-path /mnt/parity`
+export container_id=`docker ps --filter ancestor=ethcore/parity:nightly --format "{{.ID}}"`
 export signer_token=`docker exec $container_id /parity/parity signer new-token --chain kovan --base-path /mnt/parity`
-export stop_result=`docker stop $container_id`
-export remove_result=`docker rm $container_id`
 
 echo ""
 echo "<html><head><title>Parity Signer Token</title></head><body>"
